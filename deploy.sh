49e5b7c83550cc759b3abc274ee5d7e23f95df45
Kelvin Christian <kelvinchristian277@gmail.com>
2022-12-10 23:27:18 -0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ GATEWAY_CONTAINER='gateway-container'
 GATEWAY_PORT='3000'
 RATING_IMAGE='rating-image'
 RATING_CONTAINER='rating-container'
-RATING_PORT='5000'
+RATING_PORT='9000'
 PRODUCT_IMAGE='product-image'
 PRODUCT_CONTAINER='product-container'
-PRODUCT_PORT='6000'
+PRODUCT_PORT='6100'
 PURCHASE_IMAGE='purchase-image'
 PURCHASE_CONTAINER='purchase-container'
 PURCHASE_PORT='7100'
@@ -47,17 +47,17 @@ docker build -t $GATEWAY_IMAGE --target gateway-build .
 docker build -t $PRODUCT_IMAGE --target product-build .
 docker build -t $PAYMENT_IMAGE --target payment-build .
 docker build -t $PURCHASE_IMAGE --target purchase-build .
-# docker build -t $RATING_IMAGE --target rating-build .
+docker build -t $RATING_IMAGE --target rating-build .
 docker build -t $USER_MANAGEMENT_IMAGE --target user-management-build .
 
 # run the image and open the required ports
 echo "----------Running sever app----------"
-docker run -d -p $GATEWAY_PORT:$GATEWAY_PORT --name $GATEWAY_CONTAINER --network $PROJECT_NETWORK $GATEWAY_IMAGE npm start
-docker run -d -p $PRODUCT_PORT:$PRODUCT_PORT --name $PRODUCT_CONTAINER --network $PROJECT_NETWORK $PRODUCT_IMAGE PORT=$PRODUCT_PORT DB_USER=main_user DB_PASSWORD=cs6650123456 node server.js
-docker run -d -p $PAYMENT_PORT:$PAYMENT_PORT--name $PAYMENT_CONTAINER --network $PROJECT_NETWORK $PAYMENT_IMAGE PORT=$PAYMENT_PORT DB_USER=main_user DB_PASSWORD=cs6650123456 PRODUCT_PORT=$PRODUCT_PORT node server.js
-docker run -d -p $PURCHASE_PORT:$PURCHASE_PORT --name $PURCHASE_CONTAINER --network $PROJECT_NETWORK $PURCHASE_IMAGE npm start
-# docker run -d -p $RATING_PORT:$RATING_PORT --name $RATING_CONTAINER --network $PROJECT_NETWORK $RATING_IMAGE npm start
-docker run -d -p $USER_MANAGEMENT_PORT:$USER_MANAGEMENT_PORT --name $USER_MANAGEMENT_CONTAINER --network $PROJECT_NETWORK $USER_MANAGEMENT_IMAGE npm start
+docker run -d -p $GATEWAY_PORT:$GATEWAY_PORT --name $GATEWAY_CONTAINER $GATEWAY_IMAGE
+docker run -d -p $PRODUCT_PORT:$PRODUCT_PORT --name $PRODUCT_CONTAINER $PRODUCT_IMAGE
+docker run -d -p $PAYMENT_PORT:$PAYMENT_PORT --name $PAYMENT_CONTAINER $PAYMENT_IMAGE
+docker run -d -p $PURCHASE_PORT:$PURCHASE_PORT --name $PURCHASE_CONTAINER $PURCHASE_IMAGE
+docker run -d -p $RATING_PORT:$RATING_PORT --name $RATING_CONTAINER $RATING_IMAGE
+docker run -d -p $USER_MANAGEMENT_PORT:$USER_MANAGEMENT_PORT --name $USER_MANAGEMENT_CONTAINER $USER_MANAGEMENT_IMAGE
 
 echo "----------watching logs from server----------"
 echo "to view server logs use ./server_log.sh command"
